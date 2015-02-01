d0d0a805bd2e82cbed2c41ba7a1657397e079fa1
Ely M. Spears <spearsem@gmail.com>
2015-02-01 13:39:13 -0500

    Completed rough KDTree implementation with a nearest neighbor algorithm. Added Main with an example.

---
+++ weather_stations/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+import KDTree
+
+main = do
+    -- Example KD Tree from [(id, coords)]
+    let exampleTree = fromList [(1, [10, 10]), (2, [2, 4]), (3, [5, 6])]
+
+    -- Coordinates where to locate nearest station ID
+    let testPoint = [2, 8]
+
+    putStrLn $ "The closest station to " ++ show testPoint ++ " is:" 
+    putStrLn $ show $ nearestNeighbor testPoint exampleTree
