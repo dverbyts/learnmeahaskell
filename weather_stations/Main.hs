ab3ef2a7769b872c637042e71f9d22507e89c44f
Ely M. Spears <spearsem@gmail.com>
2015-02-01 13:42:19 -0500

    Updated to a more interesting test example.

--- weather_stations/Main.hs
+++ weather_stations/Main.hs
@@ -3,10 +3,13 @@ import KDTree
 
 main = do
     -- Example KD Tree from [(id, coords)]
-    let exampleTree = fromList [(1, [10, 10]), (2, [2, 4]), (3, [5, 6])]
+    let exampleTree = fromList [(1, [10, 10]), 
+                                (2, [2, 4]), 
+                                (3, [5, 6]), 
+                                (4, [-1, -1])]
 
     -- Coordinates where to locate nearest station ID
-    let testPoint = [2, 8]
+    let testPoint = [-2, -2]
 
     putStrLn $ "The closest station to " ++ show testPoint ++ " is:" 
     putStrLn $ show $ nearestNeighbor testPoint exampleTree
